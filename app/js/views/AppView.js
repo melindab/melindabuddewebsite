6d872063f44dc3fd1e5962e5f9ac525af30cebf4
melindab <melindab@users.noreply.github.com>
2017-09-17 15:59:34 -0500

    Add handlebars

--- app/js/views/AppView.js
+++ app/js/views/AppView.js
@@ -1,8 +1,9 @@
 var _ = require( 'underscore' );
 var Marionette = require( 'backbone.marionette' );
+var contentTemplate = require( '../templates/contentTemplate.hbs' );
 
 module.exports = Marionette.View.extend({
 
-    template: _.template('<h1>Welcome to my app!</h1>')
+    template: contentTemplate
 
 });
