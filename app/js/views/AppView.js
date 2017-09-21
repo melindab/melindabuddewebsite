989c9f4619a0a668050d30315637b5b177a64919
melindab <melindab@users.noreply.github.com>
2017-09-20 22:43:01 -0500

    Adjust styles

--- app/js/views/AppView.js
+++ app/js/views/AppView.js
@@ -1,9 +1,16 @@
 var _ = require( 'underscore' );
+var $ = require( 'jquery' );
 var Marionette = require( 'backbone.marionette' );
+var hammer = require( 'hammerjs' );
+var materialize = require( 'materialize-css' );
 var contentTemplate = require( '../templates/contentTemplate.hbs' );
 
 module.exports = Marionette.View.extend({
 
-    template: contentTemplate
+    template: contentTemplate,
+
+    onAttach: function() {
+        $(".button-collapse").sideNav();
+    }
 
 });
