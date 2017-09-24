a3e1af009dff6dd5bb92527c705c954493e428e8
melindab <melindab@users.noreply.github.com>
2017-09-24 02:30:08 -0500

    Add method to show content from model

--- app/js/views/MainView.js
+++ app/js/views/MainView.js
@@ -1,10 +1,14 @@
 var _ = require( 'underscore' );
 var $ = require( 'jquery' );
+var Backbone = require( 'backbone' );
 var Marionette = require( 'backbone.marionette' );
 var Radio = require( 'backbone.radio' );
 var mainTemplate = require( '../../templates/mainTemplate.hbs' );
+var MainContent = require( '../models/MainContent' );
 
 module.exports = Marionette.View.extend({
+
+    model: new Backbone.Model(),
 
     channel: Radio.channel( 'app' ),
 
@@ -12,10 +16,20 @@ module.exports = Marionette.View.extend({
 
     template: mainTemplate,
 
+    allContent: new MainContent(),
+
     initialize: function() {
+        var that = this;
+
         this.channel.on( 'navigate', function( page ) {
             console.log('event triggered and caught', page)
+            that.showContent( page );
         });
+    },
+
+    showContent: function( page ) {
+        this.model.set( this.allContent.get( 'page' + page ) );
+        this.render();
     }
 
 });
